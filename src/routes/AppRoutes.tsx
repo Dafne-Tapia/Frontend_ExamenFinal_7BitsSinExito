40b279211c7080469477d6f250143836693a9481
Dafne-Tapia <romidafne23@gmail.com>
2026-07-03 00:07:14 -0400

    diseño terminado del home, basarse en la arquitectura proporcionada, Page para el diseño del frontend que cada uno realizara en su propia carpeta, services para el consumo del backend de cada uno, cada uno debera usar obligatoriamente en el diseño los dos componentes creados footer y navbar

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../Home.tsx'
+import Servicios from '../pages/serviciosAApos/servicios.tsx'
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/su-ruta" element={<SuPagina />} /> */}
+        {/* <Route path="/servicios" element={<Servicios />} /> */}
 
       </Routes>
     </BrowserRouter>
---
+++ src/services/api.ts
@@ -0,0 +1,2 @@
+// URL base del backend desplegado en Render
+export const API_URL = "https://sevenbitssinexito-examenfinalbackend.onrender.com";
