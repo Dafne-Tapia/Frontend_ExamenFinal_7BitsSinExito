c2fd1c7babb1ee172d0dcc4e1fcd8f5de3584f22
Dafne-Tapia <romidafne23@gmail.com>
2026-07-03 16:15:53 -0400

    resolviendo requisitos para el deploy

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,6 +5,7 @@ import Documentos from '../pages/documentos/Documentos.tsx'
 import Categoria from '../pages/categoria/Categoria.tsx'
 import Procedimientos from '../pages/procedimientosAApos/procedimientos.tsx'
 import Horarios from '../pages/horariosAApos/Horarios.tsx'
+import Requisitos from '../pages/requisitos/Requisitos.tsx'
 
 
 export default function AppRoutes() {
@@ -17,6 +18,7 @@ export default function AppRoutes() {
           <Route path="/categoria" element={<Categoria />} />
           <Route path="/procedimientos" element={<Procedimientos />} />
           <Route path="/horario" element={<Horarios />} />
+          <Route path="/requisitos" element={<Requisitos />} />
           <Route path="/documentos" element={<Documentos />} />
       </Routes>
     </BrowserRouter>
