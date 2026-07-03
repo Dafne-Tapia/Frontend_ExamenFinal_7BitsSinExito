b555191b3db34200c1645bf5c7b2c26ff60c32a8
Melani Hidalgo <marcahidalgo7@gmail.com>
2026-07-03 02:12:48 -0400

    agrego ruta para procedimientos

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from '../Home.tsx'
-import Servicios from '../pages/serviciosAApos/servicios.tsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../Home";
+import Servicios from "../pages/serviciosAApos/servicios";
+import Procedimientos from "../pages/procedimientosAApos/procedimientos";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/servicios" element={<Servicios />} /> */}
-
+        <Route path="/servicios" element={<Servicios />} />
+        <Route path="/procedimientos" element={<Procedimientos />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
